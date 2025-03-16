9dd6bf098eb70ae16aada36f4ff59544d59189cd
Arnav Goel <arnavpgoel@gmail.com>
2025-03-16 16:42:37 +0530

    just trial and error

--- app/src/main/java/com/example/doctor/ArticleActivity.java
+++ app/src/main/java/com/example/doctor/ArticleActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ArticleActivity extends AppCompatActivity {
-
+//manan vbaab
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
